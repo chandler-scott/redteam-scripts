f8f15a4e36da506d8be0741683496c283b2e20d0
Chandler Scott <chandler.scott.dev@gmail.com>
2024-09-08 12:38:56 -0400

    gobuster scan

---
+++ gobuster_scan.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Check if the correct number of arguments is provided
+if [ "$#" -ne 2 ]; then
+    echo "Usage: $0 <hostname/ip> <port>"
+    exit 1
+fi
+
+# Assign variables from arguments
+HOST=$1
+PORT=$2
+
+# Wordlist to use (you can change this to any wordlist you'd like)
+WORDLIST="/usr/share/wordlists/dirb/common.txt"
+
+# Run gobuster directory scan
+gobuster dir -u http://$HOST:$PORT -w $WORDLIST -t 50 -x php,html,txt
